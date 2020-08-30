b8f4369c10019131c716f4b0ec77abf1d1c9823e
Mobina <shbmobina@gmail.com>
2020-08-30 16:18:13 +0430

    Add proposals section

--- js/init.js
+++ js/init.js
@@ -21,8 +21,8 @@ jQuery(document).ready(function($) {
       handler: function(event, direction) {
 		   var active_section;
 			active_section = $(this);
-         if (direction === "up") active_section = active_section.prev();
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+         if (direction === "up") active_section = active_section.prev().prev();
+         var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
          navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
 		},
